af8e5ccf4b7268ee9f5e1e1b7cb48bf91a8145b4
Robert Marver <ram16@uw.edu>
2019-06-10 23:07:41 -0700

    Locked in values

--- host_config.py
+++ host_config.py
@@ -1,6 +1,8 @@
 # Must change this every time servers are rebooted
 IP_MAPPINGS = {
     '35.236.24.230'  : 'California',
-    '34.76.95.24'    : 'Belgium',
-    '35.245.249.167' : 'Virginia',
+    '34.77.234.27'    : 'Belgium',
+    '35.194.70.215' : 'Virginia',
+    '35.221.109.135' : 'Japan',
+    '35.197.127.254' : 'Oregon',
 }
