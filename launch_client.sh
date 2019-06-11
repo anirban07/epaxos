90544881e7ca302ea712070de9bbe0ab6c14c515
Robert Marver <ram16@uw.edu>
2019-06-10 23:00:10 -0700

    Locked in values

--- launch_client.sh
+++ launch_client.sh
@@ -2,7 +2,8 @@
 
 cd /home/ram16/epaxos/src/clientlat
 
-CMD="go run client.go -e -maddr $1 -c $2 -q $3 -sr $4 -w $5 -fr $6 -app $7 -T 10"
+# Takes at most 4 minutes
+CMD="go run client.go -e -maddr $1 -c $2 -q 500 -w $3 -fr $4 -app $5 -T 10"
 
 echo $CMD
 
